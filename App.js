ad66cd67fbfb4db45d4975e8d5a1ba6f23963741
HK <hk540539@gmail.com>
2019-09-06 08:33:44 +0530

    change app.js for defaultnavigationoptions

--- App.js
+++ App.js
@@ -5,19 +5,23 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Home from './Routes/Home';
 import Index from './Routes/Index';
 
-const AppNavigator = createStackNavigator({
-	Home: {
-		screen: Home,
-		screenTwo: Index
+const AppNavigator = createStackNavigator(
+	{
+		Home: {
+			screen: Home,
+			screenTwo: Index
+		}
 	},
-	defaultNavigationOptions: {
-		title: 'Welcome',
-		headerTintColor: '#fff',
-		headerStyle: {
-			backgroundColor: '#BA2F16'
-		},
-		headerTitleStyle: { color: '#FFF' }
+	{
+		defaultNavigationOptions: {
+			title: 'Welcome',
+			headerTintColor: '#fff',
+			headerStyle: {
+				backgroundColor: '#BA2F16'
+			},
+			headerTitleStyle: { color: '#FFF' }
+		}
 	}
-});
+);
 const App = createAppContainer(AppNavigator);
 export default App;
